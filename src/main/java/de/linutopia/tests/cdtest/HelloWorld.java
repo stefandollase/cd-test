6eba4b0841e80c018f697d5b6f5929aa971dbe44
Stefan Dollase <stefandollase@googlemail.com>
2014-11-02 20:45:45 +0100

    added hello world

---
+++ src/main/java/de/linutopia/tests/cdtest/HelloWorld.java
@@ -0,0 +1,11 @@
+package de.linutopia.tests.cdtest;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println(new HelloWorld().message());
+	}
+
+	public String message() {
+		return "Hello World!";
+	}
+}
